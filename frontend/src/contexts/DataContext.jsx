fb609ad0e4620495388d061906bb68605e50438e
Stefan Lange <stefanlange@maeve.de>
2024-04-21 01:48:17 +0200

    DataContext init

--- frontend/src/contexts/DataContext.jsx
+++ frontend/src/contexts/DataContext.jsx
@@ -1,9 +1,24 @@
+import React, { createContext, useState } from 'react';
+import axios from 'axios';
 
+export default function DataContext({children}) {
+    const [loading, setLoading] = useState(true);
+    const [shoppingLists, setShoppingLists] = useState([]);
 
-export default function DataContext() {
+    async function fetchShoppingList() {
+        try {
+            const response = await axios.get('http://localhost:3000/api/shopping-list');
+            setShoppingLists(response.data);
+            setLoading(false);
+        } catch (error) {
+            console.error('Error fetching shopping list:', error);
+        }
+    }
+
     return (
-        <div>
-            {/* This is where the data context will go */}
-        </div>
+        <DataContext.Provider 
+        value={{ shoppingLists, setShoppingLists, loading, setLoading }}>
+            {children}
+        </DataContext.Provider>
     )
 }
