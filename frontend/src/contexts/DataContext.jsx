53c81b48fa814a98b3f8e82bbf75b2295b5f2d72
Stefan Lange <stefanlange@maeve.de>
2024-04-21 01:57:16 +0200

    context and env added

--- frontend/src/contexts/DataContext.jsx
+++ frontend/src/contexts/DataContext.jsx
@@ -4,10 +4,11 @@ import axios from 'axios';
 export default function DataContext({children}) {
     const [loading, setLoading] = useState(true);
     const [shoppingLists, setShoppingLists] = useState([]);
+    const backendUrl = import.meta.env.VITE_BACKEND_URL;
 
     async function fetchShoppingList() {
         try {
-            const response = await axios.get('http://localhost:3000/api/shopping-list');
+            const response = await axios.get(`${backendUrl}/shopping-lists`);
             setShoppingLists(response.data);
             setLoading(false);
         } catch (error) {
