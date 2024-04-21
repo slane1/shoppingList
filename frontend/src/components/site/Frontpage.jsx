9f0c31cebc3a8e577ff9f34616d8a1f4deebb864
Stefan Lange <stefanlange@maeve.de>
2024-04-21 02:03:58 +0200

    Basic structure for Frontpage

---
+++ frontend/src/components/site/Frontpage.jsx
@@ -0,0 +1,13 @@
+import Header from "./Header";
+import Welcome from "./Welcome";
+import Footer from "./Footer";
+
+export default function Frontpage() {
+    return (
+        <div>
+            <Header />
+            <Welcome />
+            <Footer />
+        </div>
+    )
+}
