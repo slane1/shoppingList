4bebf320589b0688eed738ff932ae3f3b944e9b3
Stefan Lange <stefanlange@maeve.de>
2024-04-21 16:10:09 +0200

    added first route, added site to app

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,13 +1,19 @@
 import { Routes, Route } from "react-router-dom";
+import Dashboard from "./components/Dashboard";
+import Header from "./components/site/Header";
+import Frontpage from "./components/site/Frontpage";
+import Footer from "./components/site/Footer";
 import './App.css'
 
-function App() {
-
-
+export default function App() {
   return (
-    <>
-    </>
-  )
+    <div>
+      <Header />
+      <Frontpage />
+      <Footer />
+      <Routes>
+        <Route path="/dashboard" element={<Dashboard />} />
+      </Routes>
+    </div>
+  );
 }
-
-export default App
