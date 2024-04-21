25f931eeb78254af007bb3f67d422c31d3f2a705
Stefan Lange <stefanlange@maeve.de>
2024-04-21 16:03:37 +0200

    added dashboard

---
+++ frontend/src/components/site/Dashboard.jsx
@@ -0,0 +1,16 @@
+export default function Dashboard() {
+    return (
+        <div>
+            <div>
+                <h1>Dashboard</h1>
+                <p>Welcome to the Dashboard</p>
+            </div>
+            <div>
+                <h2>Your Shopping Lists</h2>
+            </div>
+            <div>
+                <button>Create new ShoppingList</button>
+            </div>
+        </div>
+    );
+}
