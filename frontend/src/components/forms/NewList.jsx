ef4ff1917507ecccf17bbd9782f6801c5fdc9268
Stefan Lange <stefanlange@maeve.de>
2024-04-26 01:25:04 +0200

    newListComponent

---
+++ frontend/src/components/forms/NewList.jsx
@@ -0,0 +1,49 @@
+import React, { useState, useContext } from 'react';
+import { useHistory } from 'react-router-dom';
+import axios from 'axios';
+import { DataContext } from '../contexts/DataContext';
+
+export default function NewList() {
+    const { backendUrl } = useContext(DataContext);
+    const history = useHistory();
+    const [newList, setNewList] = useState({
+        title: "",
+        items: []
+    });
+
+    function handleChange(event) {
+        setNewList({
+            ...newList,
+            [event.target.name]: event.target.value
+        });
+    }
+    async function handleSubmit(event) {
+        event.preventDefault();
+        try {
+            await axios.post(`${backendUrl}/shopping-lists`, newList);
+            setNewList({
+                title: "",
+                items: []
+            });
+            fetchShoppingList();
+            history.push('/');
+        } catch (error) {
+            console.error('Error creating new list:', error);
+        }
+    }
+
+
+
+
+    return (
+        <div>
+            <div>
+                <form action="">
+                    <label htmlFor="title">Title:</label>
+                    <input type="text" id="title" name="title" value={newList.title} onChange={handleChange} />
+                    <button type='submit' onClick={handleSubmit}>Add</button>
+                </form>
+            </div>
+        </div>
+    )
+}
