c47c4834071e30024ffd1c92aeee1cb7bc303f86
Stefan Lange <stefanlange@maeve.de>
2024-04-30 02:54:24 +0200

    mail match check

--- backend/src/models/userModel.js
+++ backend/src/models/userModel.js
@@ -1,8 +1,15 @@
 import mongoose from 'mongoose';
 
 const userSchema = new mongoose.Schema({
-    name: { type: String, required: true },
-    email: { type: String, required: true, unique: true },
+    name: { type: String, required: true, unique: true},
+    email: { 
+        type: String, 
+        required: true, 
+        unique: true,
+        match: [
+        /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/,
+        'Please fill a valid email address'],
+    },
     password: { type: String, required: true },
     shoppingLists: [{ type: mongoose.Schema.Types.ObjectId, ref: 'ShoppingList' }],
 });
