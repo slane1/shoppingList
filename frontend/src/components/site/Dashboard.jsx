67a46ce384273bb14442670c72d5ffa4c36a036a
Stefan Lange <stefanlange@maeve.de>
2024-05-16 03:12:34 +0200

    lists tailwind

--- frontend/src/components/site/Dashboard.jsx
+++ frontend/src/components/site/Dashboard.jsx
@@ -23,27 +23,36 @@ export default function Dashboard() {
                 <p>Welcome to your Dashboard</p>
                 <p>Here you can view, edit and create ShoppingLists</p>
             </div>
-            <div className="flex flex-col gap-3 mt-5">
-                <h2>Your Shopping Lists</h2>
-                <ul className="flex flex-col items-center gap-5 ">
-                {shoppingLists.map((data) => (
-                    <li key={data.id} className="flex items-center text-end justify-end w-80 ">
-                        <Link
-                        to={{ pathname: `/shopping-list/${data.id}` }}
-                        state={{ data: data.id }}
-                        className="flex-grow-1"
-                        >
-                    {data.title}
-                        </Link>
-                    <button
-                        onClick={() => deleteList(data.id)}
-                        className="flex ml-5 items-center justify-center hover:text-black hover:bg-gray-700 focus:ring-gray-600">
-                        <img src={deletesvg} alt="Delete" />
-                    </button>
-                    </li>
-                ))}
-                </ul>
+{/* Start of List display container */}
+        <div className="flex justify-center">
+            <div className="mt-10 w-full max-w-md p-4 bg-white border border-gray-200 rounded-lg shadow sm:p-8 dark:bg-gray-800 dark:border-gray-700">
+                <div className="flex items-center justify-center mb-4">
+                    <h2 className="text-xl font-bold leading-none text-gray-900 dark:text-white">Your Shopping Lists</h2>
+                </div>
+                <div className="flow-root">
+                    <ul className="divide-y divide-gray-200 dark:divide-gray-700">
+                    {shoppingLists.map((data) => (
+                        <li key={data.id} className="flex justify-end py-3 sm:py-4">
+                            <Link
+                            to={{ pathname: `/shopping-list/${data.id}` }}
+                            state={{ data: data.id }}
+                            className="flex-grow-1"
+                            >
+                            {data.title}
+                            </Link>
+                        <button
+                            onClick={() => deleteList(data.id)}
+                            className="ml-5 flex justify-center items-center w-4 h-4 text-blue-700 dark:text-blue-500 bg-blue-700 hover:bg-blue-800">
+                            <img src={deletesvg} alt="Delete" />
+                        </button>
+                        </li>
+                    ))}
+                    </ul>
+                </div>
         </div>
+                
+        </div>
+{/* End of List display container */}
         <div>
             <NewList />
         </div>
